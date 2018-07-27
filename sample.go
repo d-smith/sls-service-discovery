0b3a71647c2ae55816da35bf41380dcbbc2bf5f5
d-smith <doug.smith.mail@gmail.com>
2018-07-27 14:01:33 -0700

    Added relay of get result from endpoint looked up via service discovery

--- sample.go
+++ sample.go
@@ -1,12 +1,39 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"strings"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func handleGet(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	return events.APIGatewayProxyResponse{Body: "got it\n", StatusCode: 200}, nil
+
+	cname, err := net.LookupCNAME("examplesvc.sector7.internal")
+	if err != nil {
+		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 500}, nil
+	}
+
+	if strings.HasSuffix(cname, ".") {
+		cname = strings.TrimSuffix(cname, ".")
+	}
+
+	endpoint := fmt.Sprintf("http://%s", cname)
+	fmt.Println("get", endpoint)
+
+	resp, err := http.Get(endpoint)
+	if err != nil {
+		return events.APIGatewayProxyResponse{Body: err.Error(), StatusCode: 500}, nil
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	return events.APIGatewayProxyResponse{Body: string(body), StatusCode: 200}, nil
 }
 
 func makeHandler() func(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
